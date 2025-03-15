f95ae3afa84d60522d46d7eb39d32f237596426a
JAVI <javierferfer20@gmail.com>
2025-03-15 17:06:17 +0100

    crear carpeta store; crear documento main.js; guardar documento main.js #11

---
+++ staff/javier-fernandez/playground/store/main.js
@@ -0,0 +1,37 @@
+var products = []
+
+var ciquecento = {
+    id: "202531-1601"
+    brand: "Fiat"
+    model: "500"
+    color: "pink"
+    year: 2019
+    km: 145000
+}
+products [0] = ciquecento
+    var renault = {
+        id: "2025031-1603"
+        brand: "renault"
+        model: "clio"
+        color: "red"
+        year: 2015
+        km: 225000
+    }
+products [1] Clio 
+var c3 = {
+    id: "202531-1605"
+    brand: "citroen"
+    model: "c3"
+    color: "black"
+    year: 2016
+    km: 310000
+}
+//logic
+function addcar (brand, model, color, year, km){
+    /*
+    STEPS
+    -Create id for car
+    -Create object for car and id
+    -Insert object in products
+    */
+}
