a627fea01bac5b8e03e032461f79182c1f435686
JAVI <javierferfer20@gmail.com>
2025-03-16 21:49:46 +0100

    guardar main.js #11

--- staff/javier-fernandez/playground/store/main.js
+++ staff/javier-fernandez/playground/store/main.js
@@ -1,31 +1,33 @@
 var products = []
 
 var ciquecento = {
-    id: "202531-1601"
-    brand: "Fiat"
-    model: "500"
-    color: "pink"
-    year: 2019
-    km: 145000
+    id: '2025315-1601',
+    brand: 'Fiat',
+    model: '500',
+    color: 'pink',
+    year: 2019,
+    km: 145000,
 }
 products [0] = ciquecento
     var renault = {
-        id: "2025031-1603"
-        brand: "renault"
-        model: "clio"
-        color: "red"
-        year: 2015
-        km: 225000
+        id: '20250315-1603',
+        brand: 'renault',
+        model: 'clio',
+        color: 'red',
+        year: 2015,
+        km: 225000,
     }
-products [1] Clio 
+products [1] = renault; 
+
 var c3 = {
-    id: "202531-1605"
-    brand: "citroen"
-    model: "c3"
-    color: "black"
-    year: 2016
-    km: 310000
+    id: '2025315-1605',
+    brand: 'citroen',
+    model: 'c3',
+    color: 'black',
+    year: 2016,
+    km: 310000,
 }
+products[2] = c3;
 //logic
 function addcar (brand, model, color, year, km){
     /*
@@ -35,3 +37,4 @@ function addcar (brand, model, color, year, km){
     -Insert object in products
     */
 }
+console.table(products)
