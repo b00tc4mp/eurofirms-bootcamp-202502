c102446eed35ec80c0bfae691f810a4c060b0675
Mario Garcia <mario.gaglez@gmail.com>
2025-03-16 21:18:08 +0100

    File main.js modified #3

--- staff/mario-garcia/playground/store/main.js
+++ staff/mario-garcia/playground/store/main.js
@@ -42,13 +42,13 @@ function addCar(brand, model, color, year, kilometers) {
     var car = {}
     
     car.id=Math.random() + "-" + Math.random()
-    coche.brand=brand
-    coche.model=model  
-    coche.color=color
-    coche.year=year
-    coche.kilometers=kilometers
+    car.brand=brand
+    car.model=model  
+    car.color=color
+    car.year=year
+    car.kilometers=kilometers
 
-    products[products.length] = coche
+    products[products.length] = car
 
 }
 
@@ -57,5 +57,33 @@ addCar('Smart', 'ForTwo', 'yellow', 2024, 170000)
 addCar('Fiat', '500 Abarth', 'skyblue', 2023, 56000)
 
 //view cars
+
+console.table(products)
+
+function updateCar(id, brand, model, color, year, kilometers) {
+
+/*Próximo Ejercicio - STEPS
+
+1. Iterar en products (car) y encontrar indice (posicion) en la que está el coche con ese id
+2. Extraer coche del Array (Usando ese indice)
+3. Actulizar todos los campos del coche, con los datos de entrada en la función. */
+
+for (let i = 0; i < products.length; i++) {
+
+    if(id === products[i].id) {
+        
+        let eureka = products[i]
+
+        eureka.brand = brand;
+        eureka.model = model;
+        eureka.color = color;
+        eureka.year = year;
+        eureka.kilometers = kilometers
+    }
+}
+}
+
+updateCar('150325-1650', 'Renault', 'Megane', 'green', 2010, 125000)
+updateCar('150325-1660', 'Toyota', 'Yaris', 'black', 2020, 310000)
 
 console.table(products)
