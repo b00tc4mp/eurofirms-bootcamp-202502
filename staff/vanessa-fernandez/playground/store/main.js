b9043463c92e6584cac566f4294cd0a2706453d9
mariva0280 <marivafp@gmail.com>
2025-03-17 21:11:38 +0100

    adding the updateCar function #17

--- staff/vanessa-fernandez/playground/store/main.js
+++ staff/vanessa-fernandez/playground/store/main.js
@@ -3,7 +3,7 @@ var products = []
 // populate
 
 var cinquecento = {
-    id: '2025031-1601',
+    id: '20250315-1601',
     brand: 'Fiat',
     model: '500',
     color: 'pink',
@@ -14,7 +14,7 @@ var cinquecento = {
 products[0] = cinquecento
 
 var clio = {
-    id: '2025031-1603',
+    id: '20250315-1603',
     brand: 'Renault',
     model: 'Clio',
     color: 'red',
@@ -25,7 +25,7 @@ var clio = {
 products[1] = clio
 
 var c3 = {
-    id: '2025031-1605',
+    id: '20250315-1605',
     brand: 'Citröen',
     model: 'C3',
     color: 'black',
@@ -44,17 +44,9 @@ function addCar(brand, model, color, year, kilometers) {
     -create object for car data and id
     -insert object in products
     */
-
-    
-
-
-
-
-
-
     
     // create id for car
-   var id = '2025031-' + (1601 + products.length * 2)
+   var id = '20250315-' + (1601 + products.length * 2)
    
    //create object for car data and id
    var newCar = {
@@ -71,10 +63,40 @@ function addCar(brand, model, color, year, kilometers) {
 
 }
 
+function updateCar(id, brand, model, color, year, kilometers) {
+     /*
+    STEPS
+    - iterar en products (cars) y encontrar indice (posicion) en la que está el coche con ese id
+    - extraer coche del array (usando ese indice)
+    - actualizar todos los campos del coche, con los datos de entrada en la funcion
+    */
+   for(var i = 0; i < products.length; i++) { //iterar sobre products
+    
+        var carupdate = products[i] 
+        var searchId = products[i].id
+    
+        if(searchId === id) { // comprobar la coincidencia con id, si coincide modifica el objeto
+            carupdate.brand = brand
+            carupdate.model = model
+            carupdate.color = color
+            carupdate.year = year
+            carupdate.kilometers = kilometers
+
+            console.log(`Car with ID ${id} updated successfully.`);
+            return
+        }
+    }
+}
+
+function removeCar(id) {
+    
+}
+
 addCar('Audi', 'A1', 'gray', 2013, 170000)
 addCar('Smart', 'ForTwo', 'yellow', 2024, 17000)
 addCar('Fiat', '500 Abarth', 'skyblue', 2023, 56000)
-
+updateCar('20250315-1603', 'Renault', 'Clio', 'green', 2015, 225000)
+updateCar('20250315-1605', 'Citröen', 'C3', 'black', 2012, 310000)
 // view cars
 
 console.table(products)
