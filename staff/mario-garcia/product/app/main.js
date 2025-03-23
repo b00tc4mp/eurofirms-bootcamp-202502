7707d2454585262aa45131fbf30753f40506d81b
Mario Garcia <mario.gaglez@gmail.com>
2025-03-23 21:19:48 +0100

    main.js modified #28

--- staff/mario-garcia/product/app/main.js
+++ staff/mario-garcia/product/app/main.js
@@ -1,11 +1,11 @@
 var body = document.querySelector('body')
 
-function buildLandingView () {
+function buildLandingView () { // Pintamos la Pantalla LANDING
 
     var landingView = document.createElement('div')
 
     var logoHeading = document.createElement('h1')
-    var logoText = document.createTextNode('Logo')
+    var logoText = document.createTextNode('LANDING Page')
     logoHeading.appendChild(logoText)
     landingView.appendChild(logoHeading)
 
@@ -15,7 +15,7 @@ function buildLandingView () {
     registerLink.appendChild(registerText)
     landingView.appendChild(registerLink)
 
-    var orText = document.createTextNode(' o ')
+    var orText = document.createTextNode(' or ')
     landingView.appendChild(orText)
 
     var loginLink = document.createElement('a')
@@ -30,20 +30,24 @@ function buildLandingView () {
 
 
 
-function buildRegisterView() {
+function buildRegisterView() { // Pintamos la Pantalla REGISTER
 
     var registerView = document.createElement('div')
 
     var logoHeading = document.createElement('h1')
-    var logoText = document.createElement('Logo')
+    var logoText = document.createTextNode('REGISTER Page')
     logoHeading.appendChild(logoText)
     registerView.appendChild(logoHeading)
 
     var registerForm = document.createElement('form')
+    var space = document.createElement('br')
+    var space1 = document.createElement('br')
+    var space2 = document.createElement('br')
+    var space3 = document.createElement('br')
 
     var nameField = document.createElement('div')
     var nameLabel = document.createElement('label')
-    nameLabel.htmlform = 'name'
+    nameLabel.htmlFor = 'name'
     var nameText = document.createTextNode('Name')
     nameLabel.appendChild(nameText)
     var nameInput = document.createElement('input')
@@ -51,11 +55,62 @@ function buildRegisterView() {
     nameInput.name = 'name'
     nameField.appendChild(nameLabel)
     nameField.appendChild(nameInput)
+    
+    var emailField = document.createElement('div')
+    var emailLabel = document.createElement('label')
+    emailLabel.htmlFor = 'email'
+    var emailText = document.createTextNode('Email')
+    emailLabel.appendChild(emailText)
+    var emailInput = document.createElement('input')
+    emailInput.type = 'text'
+    emailInput.name = 'email'
+    emailField.appendChild(emailLabel)
+    emailField.appendChild(emailInput)
+
+    var usernameField = document.createElement('div')
+    var usernameLabel = document.createElement('label')
+    usernameLabel.htmlFor = 'username'
+    var usernameText = document.createTextNode('Username')
+    usernameField.appendChild(usernameText)
+    var usernameInput = document.createElement('input')
+    usernameInput.type = 'text'
+    usernameInput.name = 'username'
+    usernameField.appendChild(usernameLabel)
+    usernameField.appendChild(usernameInput)
+
+    var passwordField = document.createElement('div')
+    var passwordLabel = document.createElement('label')
+    passwordLabel.htmlFor = 'password' // ¿Que estamos haciendo aquí?
+    var passwordText = document.createTextNode('Password')
+    passwordField.appendChild(passwordText)
+    var passwordInput = document.createElement('input')
+    passwordInput.type = 'text'
+    passwordInput.name = 'password'
+    passwordField.appendChild(passwordLabel)
+    passwordField.appendChild(passwordInput)
+
+
+
+    //A continuación PINTAMOS los CAMPOS en la Pantalla REGISTER = var registerView
+
     registerForm.appendChild(nameField)
+    registerForm.appendChild(space)
+    registerForm.appendChild(emailField)
+    registerForm.appendChild(space1)
+    registerForm.appendChild(usernameField)
+    registerForm.appendChild(space2)
+    registerForm.appendChild(passwordField)
 
     // TODO Finish Register View
 
     registerView.appendChild(registerForm)
+    registerForm.appendChild(space3)
+
+    var registerButton = document.createElement('button')
+    var registerText = document.createTextNode('Register')
+    registerButton.style.backgroundColor = 'blue'
+    registerButton.appendChild(registerText)
+    registerView.appendChild(registerButton)
 
     return registerView
 }
@@ -65,6 +120,9 @@ function buildHomeView() {} // TODO Implement
 
 // var landingView = buildLandingView()
 // body.appendChild(landingView)
+
+var landingView = buildLandingView()
+body.appendChild(landingView)
 
 var registerView = buildRegisterView()
 body.appendChild(registerView)
