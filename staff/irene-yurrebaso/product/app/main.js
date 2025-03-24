df1d025d8ea99e5f84fa7293a5a36ea749f4047e
Irene12345 <15231820+Irene12345@users.noreply.github.com>
2025-03-24 21:18:19 +0100

    implement landing, register, login & home views #27

---
+++ staff/irene-yurrebaso/product/app/main.js
@@ -0,0 +1,215 @@
+//traer el objeto body (como ya existe en el documento html lo aprovechamos sin crearlo de nuevo)
+var body = document.querySelector('body')
+
+//metemos el contenido de cada pagina en una funcion y despues la invocamos
+function buildLandingView() {
+    //crear un div para la pagina de landing view
+    var landingView = document.createElement('div')
+
+    //crear el logo: 1. crear etiqueta html, 2. crear texto, 3. poner texto dentro de etiqueta
+    var logoHeading = document.createElement('h1')
+    var logoText = document.createTextNode('Logo')
+    logoHeading.appendChild(logoText)
+    //ensamblar logoHeading dentro de landingView
+    landingView.appendChild(logoHeading) 
+
+    //crear "Register": 1. etiqueta html (con attr), 2. texto, 3. poner texto en etiqueta
+    var registerLink = document.createElement('a')
+    registerLink.href = "#"
+    var registerText = document.createTextNode('Register')
+    registerLink.appendChild(registerText)
+    //ensamblar Register en div landingView
+    landingView.appendChild(registerLink)
+
+    //crear "or" y ensamblar en div landingView ya que es hijo directo de este elemento
+    var orText = document.createTextNode(' or ')
+    landingView.appendChild(orText)
+
+    //crear "Login": 1. etiqueta html (con attr), 2.texto link, 3. texto dentro de etiqueta
+    var loginLink = document.createElement('a')
+    loginLink.href = '#'
+    var loginText = document.createTextNode('Login')
+    loginLink.appendChild(loginText)
+    //ensamblar Login en div landingView
+    landingView.appendChild(loginLink)
+
+    return landingView
+}
+
+function buildRegisterView() {
+    var registerView = document.createElement('div')
+
+    //logo
+    var logoHeading = document.createElement('h1')
+    var logoText = document.createTextNode('Logo')
+    logoHeading.appendChild(logoText)
+    registerView.appendChild(logoHeading) 
+
+    //crear formulario
+    var registerForm = document.createElement('form')
+
+    //crear Label y añadirle for attribute y su texto
+    var nameLabel = document.createElement('label')
+    nameLabel.htmlFor = 'name'
+    var nameText = document.createTextNode('Name')
+    nameLabel.appendChild(nameText)
+    //crear Input y añadirle type & name attributes
+    var nameInput = document.createElement('input')
+    nameInput.type = 'text'
+    nameInput.name = 'name'
+    //poner cada pareja de label & input en un div
+    var nameField = document.createElement('div')
+    nameField.appendChild(nameLabel)
+    nameField.appendChild(nameInput)
+    //ensamblar pareja en formulario
+    registerForm.appendChild(nameField)
+
+    //label & input para e-mail
+    var emailLabel = document.createElement('label')
+    emailLabel.htmlFor = 'email'
+    var emailText = document.createTextNode('E-mail')
+    emailLabel.appendChild(emailText)
+    var emailInput = document.createElement('input')
+    emailInput.type = 'text'
+    emailInput.name = 'email'
+    var emailField = document.createElement('div')
+    emailField.appendChild(emailLabel)
+    emailField.appendChild(emailInput)
+    registerForm.appendChild(emailField)
+
+    //label & input para username
+    var usernameLabel = document.createElement('label')
+    usernameLabel.htmlFor = 'username'
+    var usernameText = document.createTextNode('Username')
+    usernameLabel.appendChild(usernameText)
+    var usernameInput = document.createElement('input')
+    usernameInput.type = 'text'
+    usernameInput.name = 'username'
+    var usernameField = document.createElement('div')
+    usernameField.appendChild(usernameLabel)
+    usernameField.appendChild(usernameInput)
+    registerForm.appendChild(usernameField)
+
+    //label & input para password
+    var passwordLabel = document.createElement('label')
+    passwordLabel.htmlFor = 'password'
+    var passwordText = document.createTextNode('Password')
+    passwordLabel.appendChild(passwordText)
+    var passwordInput = document.createElement('input')
+    passwordInput.type = 'text'
+    passwordInput.name = 'password'
+    var passwordField = document.createElement('div')
+    passwordField.appendChild(passwordLabel)
+    passwordField.appendChild(passwordInput)
+    registerForm.appendChild(passwordField)
+
+    //register button
+    var button = document.createElement('button')
+    var buttonText = document.createTextNode('Register')
+    button.appendChild(buttonText)
+    button.type = 'submit'
+    registerForm.appendChild(button)
+
+    //login link
+    var loginLink = document.createElement('a')
+    loginLink.href = '#'
+    var loginLinkText = document.createTextNode('Login')
+    loginLink.appendChild(loginLinkText)
+    registerForm.appendChild(loginLink)
+
+    //ensamblar formulario en div registerView
+    registerView.appendChild(registerForm)
+
+    return registerView
+}
+
+function buildLoginView() {
+    var loginView = document.createElement('div')
+
+    //logo
+    var logoHeading = document.createElement('h1')
+    var logoText = document.createTextNode('Logo')
+    logoHeading.appendChild(logoText)
+    loginView.appendChild(logoHeading)
+
+    //crear formulario
+    var loginForm = document.createElement('form')
+
+    //label & input para username
+    var usernameLabel = document.createElement('label')
+    usernameLabel.htmlFor = 'username'
+    var usernameText = document.createTextNode('Username')
+    usernameLabel.appendChild(usernameText)
+    var usernameInput = document.createElement('input')
+    usernameInput.type = 'text'
+    usernameInput.name = 'username'
+    var usernameField = document.createElement('div')
+    usernameField.appendChild(usernameLabel)
+    usernameField.appendChild(usernameInput)
+    loginForm.appendChild(usernameField)
+
+    //label & input para password
+    var passwordLabel = document.createElement('label')
+    passwordLabel.htmlFor = 'password'
+    var passwordText = document.createTextNode('Password')
+    passwordLabel.appendChild(passwordText)
+    var passwordInput = document.createElement('input')
+    passwordInput.type = 'text'
+    passwordInput.name = 'password'
+    var passwordField = document.createElement('div')
+    passwordField.appendChild(passwordLabel)
+    passwordField.appendChild(passwordInput)
+    loginForm.appendChild(passwordField)
+
+    //login button
+    var button = document.createElement('button')
+    var buttonText = document.createTextNode('Login')
+    button.appendChild(buttonText)
+    button.type = 'submit'
+    loginForm.appendChild(button)
+
+    //register link
+    var registerLink = document.createElement('a')
+    registerLink.href = '#'
+    var registerLinkText = document.createTextNode('Register')
+    registerLink.appendChild(registerLinkText)
+    loginForm.appendChild(registerLink)
+
+    //ensamblar formulario en div loginView
+    loginView.appendChild(loginForm)
+
+    return loginView
+}
+
+function buildHomeView() {
+    var homeView = document.createElement('div')
+
+    //logo
+    var logoHeading = document.createElement('h1')
+    var logoText = document.createTextNode('Logo')
+    logoHeading.appendChild(logoText)
+    homeView.appendChild(logoHeading)
+
+    //hello home
+    var headingHome = document.createElement('h2')
+    var headingHomeText = document.createTextNode('Hello, Home!')
+    headingHome.appendChild(headingHomeText)
+    homeView.appendChild(headingHome)
+
+    return homeView
+}
+
+//llamar a la funcion y ensamblar los div contenedores de cada xView dentro de body
+
+var landingView = buildLandingView()
+body.appendChild(landingView)
+
+//var registerView = buildRegisterView()
+//body.appendChild(registerView)
+
+//var loginView = buildLoginView()
+//body.appendChild(loginView)
+
+//var homeView = buildHomeView()
+//body.appendChild(homeView)
+
