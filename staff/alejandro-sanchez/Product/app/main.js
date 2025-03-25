adc0480a769aa6a1f9dafb2b48182d5f735cb63d
Alejandro Sanchez <asbrelationwork@gmail.com>
2025-03-26 00:21:48 +0100

    staff/alejandro-sanchez/Product

---
+++ staff/alejandro-sanchez/Product/app/main.js
@@ -0,0 +1,24 @@
+var body = document.querySelector('body')
+
+var landingView = document.createElement('div')
+
+var logoHeading = document.createElement('h1')
+var logoText = document.createTextNode('Logo')
+logoHeading.appendChild(logoText)
+landingView.appendChild(logoHeading)
+
+var registerLink = document.createElement('a')
+registerLink.href = '#'
+var registerText = document.CreateTextNode('Register')
+registerLink.appendChild(registerLinkText)
+
+var orText = document.createElement('a')
+loginlink.ref ='#'
+var loginText = document.createTextNode('Login')
+loginLing.appendChild(loginText)
+landingView.appendChild(loginlink)
+
+
+
+body.appendChild(landingView)
+
