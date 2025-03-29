a75cf6cd94dc767fd9f57c659301e96d7f168d14
JAVI <javierferfer20@gmail.com>
2025-03-29 17:36:10 +0100

    actualizar logic.js #32

---
+++ staff/javier-fernandez/product/app/logic.js
@@ -0,0 +1,15 @@
+function registerUser(name, email, useranme, password) {
+    if (typeof name ¡== "string") throw new Error("invalid name type")
+    if (name.length < 1) throw new Error("invalid name length")
+    if (name.length > 30) throw new Error("invalid name max length")
+    if (typeof email ¡== "string") throw new Error("invaled email type")
+    if (email.length < 6) throw new Error ("invalid email length")
+    if (email.length > 30) throw new Error("invalid email max length")
+    if (typeof username ¡== "string") throw new Error("invalid username type")
+    if (username.length < 3) throw new Error("invalid username length")
+    if (username.length > 20) throw new Error("invalid username max lenth")
+    if (typeof password ¡== "string") throw new Error ("invalid password type")
+    if (password.length < 8) throw new Error("invalid password length")
+    if (password.length > 20) throw new Error("invalid password max length")
+        //TO DO REGISTER USER IN DATABASE
+}
