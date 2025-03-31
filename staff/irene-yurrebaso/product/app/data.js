f2496a5e5553221d990ee2cef049a781e990728d
Irene12345 <15231820+Irene12345@users.noreply.github.com>
2025-03-31 10:30:24 +0200

    started adding logic and data #27

---
+++ staff/irene-yurrebaso/product/app/data.js
@@ -0,0 +1,5 @@
+//creamos un array donde vamos a ir guardando a los usuarios
+var users = [
+    { name: 'Wendy Darling', email: 'wendy@darling.com', username: 'wendydarling', password: '12334566'},
+    { name: 'Peter Pan', email: 'peter@pan.com', username: 'peterpan', password: '23654789'}
+]
