e5c5a3a337a2c3c62c99cd175ab3a5f65469425f
manuelbarzi <manuelbarzi@gmail.com>
2025-04-05 18:07:53 +0200

    implement landing register login and home in react #25

--- staff/manuel-barzi/product/app/App.jsx
+++ staff/manuel-barzi/product/app/App.jsx
@@ -1,17 +1,18 @@
+import { Landing } from './view/Landing'
+import { Register } from './view/Register'
+import { Login } from './view/Login'
+import { Home } from './view/Home'
+
 export const App = () => {
     console.log('App -> render')
 
     return <>
-        <h1>Hola, App!</h1>
+        <Landing />
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam similique vel quidem corrupti omnis? Dignissimos, soluta, ad temporibus voluptate asperiores quibusdam voluptatum ex quam esse aspernatur ipsum iste rem corrupti?</p>
+        {/* <Register /> */}
 
-        <ul>
-            <li>red</li>
-            <li>green</li>
-            <li>blue</li>
-            <li>violet</li>
-            <li>yellow</li>
-        </ul>
+        {/* <Login /> */}
+
+        {/* <Home /> */}
     </>
 }
