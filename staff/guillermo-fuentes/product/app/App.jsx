2475722c3ce75b77b671abaab423db93dc1a4ff6
Guillermo <fuentesbuenosvinosguillermo@gmail.com>
2025-04-05 17:04:27 +0200

    organizar carpetas #26

---
+++ staff/guillermo-fuentes/product/app/App.jsx
@@ -0,0 +1,21 @@
+export const App = () => {
+  return (
+    <>
+      <h1>Hola App</h1>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur quis consequatur, soluta temporibus
+        dignissimos modi nemo rerum laborum ex obcaecati, libero odio sapiente dolor totam corporis iste saepe, aliquid
+        adipisci. Distinctio veritatis commodi obcaecati adipisci sunt. Non, voluptates dicta maiores earum quis, fugiat
+        quos atque ullam repudiandae tenetur nesciunt sed minus natus ducimus possimus repellendus, delectus
+        reprehenderit provident blanditiis perferendis! Dolorum vitae, vel quasi impedit maxime ad sunt non aliquam, aut
+        molestias possimus et ex quam esse. Laudantium enim ipsam rerum aliquam quos inventore ullam minus, atque,
+        tempore magnam odio! Sequi voluptatibus delectus atque! Dolores, accusamus possimus nobis assumenda nisi sunt ea
+        illo cum totam ipsum quo molestias commodi beatae nostrum obcaecati omnis porro earum. Eligendi recusandae illo
+        fuga incidunt. Earum dolores est vero beatae aperiam, totam magnam deserunt, illum voluptates reprehenderit
+        iusto quidem eligendi accusantium placeat praesentium! Voluptates quam at ad eum, veniam inventore voluptatibus
+        quas recusandae quia error?
+      </p>
+    </>
+  );
+};
