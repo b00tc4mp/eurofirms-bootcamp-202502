b532473cf9e27068dd6c281eb1523f11cd16c78f
soledad <soledad@exa,ple.comgit config --global user.name soledadgit status -ugit statusgit push git statusgit config --global user.email soledad@exa,ple.com>
2025-04-05 18:20:45 +0200

    modificacion carpeta Add #35

---
+++ staff/soledad-armayones/product/app/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+    console.log('App -> render')
+
+    return <>
+        <h1>Hola,App!</h1>
+        <p>Lorem ipsum dolor sit amet, conse</p>
+
+        <ul>
+            <li>red</li>
+            <li>green</li>
+            <li>blue</li>
+            <li>violet</li>
+            <li>yellow</li>
+        </ul>
+
+    </>
+}
---
+++ staff/soledad-armayones/product/zzz/app.2/data.js
@@ -0,0 +1,6 @@
+var users = [
+    { name: "Sole", username: "Armay", email: "Sole@gmail.com", password: "123123123" },
+
+    { name: "Jesus", username: "Rodriguez", email: "Jesus@gmail.com", password: "123123123" }
+
+]
