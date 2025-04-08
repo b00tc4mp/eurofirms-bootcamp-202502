ed1e00034fbe2a02b994d969876c172ff1c61505
Laura Anton <lauraamps@gmail.com>
2025-04-08 20:31:24 +0200

    subo cambios 08-04 #23

--- staff/Laura-Anton/product/app/data.js
+++ staff/Laura-Anton/product/app/data.js
@@ -1,4 +1,4 @@
-var users = [ 
+const users = [ 
     {name: "Laura", username: "LA", email: "LA@gmail.com", password: "1234"},
 
     {name: "Javi", username: "JM", email: "Ja@gmail.com", password: "1234"}
