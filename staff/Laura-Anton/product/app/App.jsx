78dbdee40a14624e5f8a73d6d4443a551d7f6faa
Laura Anton <lauraamps@gmail.com>
2025-04-08 20:29:20 +0200

    subo carpeta product 08-04 #23

---
+++ staff/Laura-Anton/product/app/App.jsx
@@ -0,0 +1,20 @@
+import { Landing } from './view/Landing'
+import { Register } from './view/Register'
+import { Login } from './view/Login'
+import { Home } from './view/Home'
+
+
+export const App = () => {
+    console.log('App -> render')
+
+    return <>
+    {/*<Landing />*/}
+
+    { /*<Register />*/ }
+
+        {/*<Login />*/ }
+
+        {/*<Home /> */}
+        </>
+
+}
---
+++ staff/Laura-Anton/product/app/view/Home.jsx
@@ -0,0 +1,11 @@
+export const Home =() => {
+    console.log('Home -> render')
+
+    return <div className= "p-20px">
+        <i>Logo</i>
+
+        <h1>Hello, World!</h1>
+
+        <button className="black-button">Logout</button>
+        </div>
+}
