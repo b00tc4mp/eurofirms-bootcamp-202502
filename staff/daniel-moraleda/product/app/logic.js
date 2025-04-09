1b0db8f4f79e5626b6ace9c7c70aace4b88aece1
Daniel Moraleda Robles <danielmoraledarobles@gmail.com>
2025-04-09 21:55:22 +0200

    updated to version 2.0 app #39

--- staff/daniel-moraleda/product/app/logic.js
+++ staff/daniel-moraleda/product/app/logic.js
@@ -4,13 +4,13 @@ function registerUser(name, email, username, password) {
     if (name.length > 30) throw new Error('invalid name length')
     if (typeof email !== 'string') throw new Error('invalid email type')
     if (email.length < 6) throw new Error('invalid email length')
-    if (email.length < 30) throw new Error('invalid email length')
+    if (email.length > 30) throw new Error('invalid email length')
     if (typeof username !== 'string') throw new Error('invalid username type')
     if (username.length < 3) throw new Error('invalid username length')
-    if (username.length < 30) throw new Error('invalid username length')
+    if (username.length > 30) throw new Error('invalid username length')
     if (typeof password !== 'string') throw new Error('invalid password type')
     if (password.length < 8) throw new Error('invalid password length')
-    if (password.length < 30) throw new Error('invalid password length')
+    if (password.length > 30) throw new Error('invalid password length')
     //TODO register user 
 
 
@@ -28,4 +28,28 @@ function registerUser(name, email, username, password) {
     }
 
     user[user.length] = user
+}
+
+function loginUser(username, password) {
+    if (typeof username !== 'string') throw new Error('invalid username type')
+    if (username.length < 3) throw new Error('invalid username length')
+    if (username.length > 30) throw new Error('invalid username length')
+    if (typeof password !== 'string') throw new Error('invalid password type')
+    if (password.length < 8) throw new Error('invalid password length')
+    if (password.length > 30) throw new Error('invalid password length')
+
+    let user
+
+    for (let i = 0; i < users.length; i++) {
+        const _user = users[i]
+
+        if (_user.username === username) {
+            user = _user
+
+            break
+        }
+    }
+    if (user === undefined) throw new Error('user not found')
+
+    if (user.password !== password) throw new Error('wrong credentials')
 }
---
+++ staff/daniel-moraleda/product/app/view/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    console.log('Home -> render')
+
+    return <div className="p-20px">
+        <i>Logo</i>
+
+        <button className="black-buttton">Logout</button>
+    </div>
+}
