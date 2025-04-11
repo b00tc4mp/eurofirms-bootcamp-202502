d17d179d818bdda31f0a25ff9c93c34bb092bace
JAVI <javierferfer20@gmail.com>
2025-04-11 22:13:01 +0200

    actualizar Home.jsx y Landing.jsx #32

--- staff/javier-fernandez/product/app/view/Landing.jsx
+++ staff/javier-fernandez/product/app/view/Landing.jsx
@@ -1,7 +1,7 @@
 export const Landing = () => {
     console.log("Landing -> render")
-    return <div className="p.20px">
-        <i className="block">Logo</i>
+    return <div className="p-5">
+        <i className="text-5xl">Logo</i>
 
         <div className="mt-10px">
             <a href="#">Register</a> or <a href="#">Login</a>
