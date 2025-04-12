0573f622c813fcc760c368a62bf481a2ce626e6d
Jose Prieto <japrieto2@hotmail.com>
2025-04-12 07:06:08 +0200

    modificar view #30

--- staff/jose-prieto/product/app/view/Landing.jsx
+++ staff/jose-prieto/product/app/view/Landing.jsx
@@ -1,13 +1,12 @@
 
+export const Landing = () => {
     console.log('Landing -> render')
-    export const Landing = () => {
-        console.log('Landing -> render')
-    
-        return <div className="p-20px">
-            <i className="block">Logo</i>
-    
-            <div className="mt-10px">
-                <a href="#">Register</a> or <a href="#">Login</a>
-            </div>
+
+    return <div className="p-5">
+        <i className="text-2xl">Logo</i>
+
+        <div className="mt-2">
+            <a href="#" className="underline">Register</a> or <a href="#" className="underline">Login</a>
         </div>
-    }
+    </div>
+}
