5024f717ca9093b76dc3c2cb592036a96b1e4562
Jose Prieto <japrieto2@hotmail.com>
2025-04-12 16:06:23 +0200

    modificar rutas #30

--- staff/jose-prieto/product/app/data.js
+++ staff/jose-prieto/product/app/data.js
@@ -1,9 +1,11 @@
 var users = [
     {
-        name: 'pepe', email: 'pepe@gmail.com', username: 'pepe', password: '1234'
+        name: 'pepe', email: 'pepe@gmail.com', username: 'pepe', password: '12345678'
     },
     {
-        name: 'lola', email: 'lola@gmail.com', username: 'lola', password: '4321'
+        name: 'lola', email: 'lola@gmail.com', username: 'lola', password: '87654321'
     }
 ]
-
+export const data = {
+    users
+}
--- staff/jose-prieto/product/app/logic.js
+++ staff/jose-prieto/product/app/logic.js
@@ -1,30 +1,29 @@
-function registerUser(name, email, username, password) {
+import { data } from './data'
+
+// function registerUser(name, email, username, password)
+// const registerUser = function (name, email, username, password) {
+const registerUser = (name, email, username, password) => {
     if (typeof name !== 'string') throw new Error('invalid name type')
     if (name.length < 1) throw new Error('invalid name min length')
     if (name.length > 30) throw new Error('invalid name max length')
-
 
     if (typeof email !== 'string') throw new Error('invalid email type')
     if (email.length < 6) throw new Error('invalid email min length')
     if (email.length > 30) throw new Error('invalid email max length')
 
-
     if (typeof username !== 'string') throw new Error('invalid username type')
     if (username.length < 3) throw new Error('invalid username min length')
     if (username.length > 20) throw new Error('invalid username max length')
-
 
     if (typeof password !== 'string') throw new Error('invalid password type')
     if (password.length < 8) throw new Error('invalid password min length')
     if (password.length > 20) throw new Error('invalid password max length')
 
-
-    for (let i = 0; i < users.length; i++) {
-        const user = users[i]
+    for (let i = 0; i < data.users.length; i++) {
+        const user = data.users[i]
 
         if (user.email === email || user.username === username) throw new Error('user already exists')
     }
-
 
     const user = {
         name: name,
@@ -33,12 +32,12 @@ function registerUser(name, email, username, password) {
         password: password
     }
 
-
-    users[users.length] = user
+    data.users[data.users.length] = user
 }
 
-
-function loginUser(username, password) {
+// function loginUser(username, password) {
+// const loginUser = function (username, password) {
+const loginUser = (username, password) => {
     if (typeof username !== 'string') throw new Error('invalid username type')
     if (username.length < 3) throw new Error('invalid username min length')
     if (username.length > 20) throw new Error('invalid username max length')
@@ -49,8 +48,8 @@ function loginUser(username, password) {
 
     let user
 
-    for (let i = 0; i < users.length; i++) {
-        const _user = users[i]
+    for (let i = 0; i < data.users.length; i++) {
+        const _user = data.users[i]
 
         if (_user.username === username) {
             user = _user
@@ -62,4 +61,9 @@ function loginUser(username, password) {
     if (user === undefined) throw new Error('user not found')
 
     if (user.password !== password) throw new Error('wrong credentials')
+}
+
+export const logic = {
+    registerUser,
+    loginUser
 }
