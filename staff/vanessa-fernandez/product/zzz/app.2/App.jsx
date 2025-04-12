b987a441ca86956a6f787371122b8a3921ab3192
mariva0280 <marivafp@gmail.com>
2025-04-12 13:26:24 +0200

    add version app.2 to implement useState navigation #43

---
+++ staff/vanessa-fernandez/product/zzz/app.2/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Landing } from './view/Landing'
+import { Register } from './view/Register'
+import { Login } from './view/Login'
+import { Home } from './view/Home'
+
+export const App = () => {
+    const [view, setView] = useState('landing')
+
+    const handleRegisterClick = () => setView('register')
+
+    const handleLoginClick = () => setView ('login')
+
+    console.log('App -> render')
+
+    return <>
+        { view === 'landing' &&  <Landing  onRegisterClick ={handleRegisterClick} onLoginClick = {handleLoginClick} /> }
+        { view === 'register' && <Register onLoginClick = {handleLoginClick} /> }
+        { view === 'login' && <Login onRegisterClick = {handleRegisterClick} /> }
+        { view === 'home' && <Home /> }
+
+    </>
+}
---
+++ staff/vanessa-fernandez/product/zzz/app.2/view/Login.jsx
@@ -0,0 +1,30 @@
+export const Login = (props) => {
+    const onRegisterClick = props.onRegisterClick
+
+    const handleRegisterClick = () => onRegisterClick()
+    console.log('Login -> render')
+
+    return <div className="p-20px">
+
+        <i>Logo</i>
+
+        <h1>Login page</h1>
+        <form className="flex flex-col gap-20px">
+
+            <div className="flex flex-col gap-10px">
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" id="username" placeholder="Your username" />
+            </div>
+
+            <div className="flex flex-col gap-10px">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Your password" />
+            </div>
+
+            <div className="flex justify-between">
+                <a href="#" onClick={handleRegisterClick}>Register</a>
+                <button className="black-button" type="submit">Login</button>
+            </div>
+        </form>
+    </div>
+}
