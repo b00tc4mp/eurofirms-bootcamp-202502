1b079680c009433850d1cce35ccd8d11bf3e7c22
Jose Prieto <japrieto2@hotmail.com>
2025-04-12 06:57:11 +0200

    actualizar view #30

--- staff/jose-prieto/product/app/view/Home.jsx
+++ staff/jose-prieto/product/app/view/Home.jsx
@@ -1,11 +1,11 @@
-export const Home = () =>{
-    console.log('Home -> reder')
+export const Home = () => {
+    console.log('Home -> render')
 
     return <div className="p-20px">
         <i>Logo</i>
 
         <h1>Hello, World!</h1>
 
-        <button className="black-button"></button>
-        </div>
+        <button className="black-button">Logout</button>
+    </div>
 }
