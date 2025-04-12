b38e882d4d36cad6082c56c6b4a90d5811d38757
JAVI <javierferfer20@gmail.com>
2025-04-12 12:33:32 +0200

    crear app2 y moverlo a zzz #32

--- staff/javier-fernandez/product/app/App.jsx
+++ staff/javier-fernandez/product/app/App.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react"
+
 import { Landing } from "./view/Landing"
-//import { Register } from "./view/Register"
-//import { Loging } from "./view/Login"
-//import { Home } from "./view/Home"
+import { Register } from "./view/Register"
+import { Loging } from "./view/Login"
+import { Home } from "./view/Home"
+
 
 export const App = () => {
+    // const viewState = useState{"landing"}
+    // const view = viewState[0]
+    // const setView = viewState[1]
+    // const [view, setView] = useState{"landing"}
+
+    const {count, setCount} = useState(0)
+
+    const handleCounterClick = () => setCount(count + 1)
+
     console.log("App -> render")
 
     return <>
+        <button className="border px-5 bg-blue-500 text-white cursor-pointer"
+            onClick={handleCounterClick}
+        >
+            {count}
+            </button>
+
         <Landing/>
-        {/* <Register /> */}
+        { //  view === "landing" && <Landing/> }
 
-        {/* <Login /> */}
+        // { view === "register" && <Register />}
 
-        {/* <Home />} */}
+        //{ view === "login" && <Login /> }
+
+        { // view == "home" && <Home />} 
     </>
 }
---
+++ staff/javier-fernandez/product/zzz/app.2/App.jsx
@@ -0,0 +1,17 @@
+import { Landing } from "./view/Landing"
+//import { Register } from "./view/Register"
+//import { Loging } from "./view/Login"
+//import { Home } from "./view/Home"
+
+export const App = () => {
+    console.log("App -> render")
+
+    return <>
+        <Landing/>
+        {/* <Register /> */}
+
+        {/* <Login /> */}
+
+        {/* <Home />} */}
+    </>
+}
