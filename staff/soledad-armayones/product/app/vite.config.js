834372854a41203c715b0dc8524078de58d5b849
soledad <soledad@exa,ple.comgit config --global user.name soledadgit status -ugit statusgit push git statusgit config --global user.email soledad@exa,ple.com>
2025-04-13 18:28:48 +0200

    instalacion vite #35

--- staff/soledad-armayones/product/app/vite.config.js
+++ staff/soledad-armayones/product/app/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react'
-
+import react from '@vitejs/lugin-react'
+import tailwindcss from @tailwindss / vite
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react(), tailwindcss()]
 })
