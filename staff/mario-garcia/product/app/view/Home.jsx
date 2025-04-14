b906223ecf162d897c61d8a9d7cc28d1925feed0
Mario Garcia <mario.gaglez@gmail.com>
2025-04-14 21:29:22 +0200

    Name into Home Page added #28

--- staff/mario-garcia/product/app/view/Home.jsx
+++ staff/mario-garcia/product/app/view/Home.jsx
@@ -48,7 +48,7 @@ export const Home = props => {
 
             <h1 className="text-3xl text-blue-900 font-semibold py-2">HOME Page</h1>
 
-            <h2 className="text-xl text-orange-700 py-2">Hello, World!</h2>
+            <h2 className="text-xl text-orange-700 py-2">Hello, {username}!</h2>
 
             <button className="border-1 rounded-md bg-orange-50 text-orange-900 px-2 font-medium"
                 type="button"
