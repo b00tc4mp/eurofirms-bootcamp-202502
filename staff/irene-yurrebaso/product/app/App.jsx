01b80802f62d633182239e5e9a6d3acf37e13c2c
Irene12345 <15231820+Irene12345@users.noreply.github.com>
2025-04-14 13:04:15 +0200

    add react hook use-state in app #27

--- staff/irene-yurrebaso/product/app/App.jsx
+++ staff/irene-yurrebaso/product/app/App.jsx
@@ -1,19 +1,29 @@
+// importamos el hook useState, lo utilizaremos para encender y apagar vistas; useState es una funcion que devuelve un array y crea un estado sobre un dato
+import { useState } from 'react'
+
 import { Landing } from './view/Landing'
 import { Register } from './view/Register'
 import { Login } from './view/Login'
 import { Home } from './view/Home'
 
 export const App = () => {
+    //inicialmente al useState le damos el valor 'landing'
+    const viewState = useState('landing')
+    //viewState va a devolver un array con 2 posiciones, el 1ro tiene el valor actual, y el 2do tiene una funcion que permite cambiar estado:
+    const view = viewState[0]
+    const setView = viewState[1]
+
     console.log('App -> render')
 
     //vite esta convirtiendo estos componentes html a javascript DOM y mostrandolos en el div root
     return <>
-        <Landing />
+        {/* muestrame este componente Landing si view es 'landing' */}
+        {view === 'landing' && <Landing />}
 
-        {/* <Register /> */}
+        {view === 'register' && <Register />}
 
-        {/* <Login /> */}
+        {view === 'login' && <Login />}
 
-        {/* <Home /> */}
+        {view === 'home' && <Home />}
     </>
 }
