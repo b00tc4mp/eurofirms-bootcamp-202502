d2c728dadae303f7d370ef7d10e79c1902f7e197
JAVI <javierferfer20@gmail.com>
2025-04-15 20:57:07 +0200

    modificar data.js #32

--- staff/javier-fernandez/product/app/data.js
+++ staff/javier-fernandez/product/app/data.js
@@ -1,8 +1,43 @@
-var users =[
-    { name: "wendy Darling", email: "wendy@darling.com", username: "wendydarling", password: "123123123 "},
-    {name: "Peter Pan", email: "peter@pan.com", username: "peterpan", password: "123123123" }
-]
-
 export const data = {
-    users
-// }
+    users: [],
+    usersCount: 0,
+
+    posts: [],
+    postsCount: 0.
+
+    userId: null
+}
+
+// populate users
+
+data.usersCount++
+
+data.users.push({
+    id: 'user-' + data.usersCount,
+    name: 'Wendy Darling',
+    email: 'wendy@darling.com',
+    username: 'wendydarling',
+    password: '123123123'
+})
+
+data.usersCount++
+
+data.users.push({
+    id: 'user-' + data.usersCount,
+    name: 'Peter Pan',
+    email: 'peter@pan.com',
+    username: 'peterpan',
+    password: '123123123'
+})
+
+data.usersCount++
+
+data.users.push({
+    id: 'user-' + data.usersCount,
+    name: 'James Hoock',
+    email: 'james@hoock',
+    username: 'jameshoock',
+    password: '123123123'
+})
+
+// populate posts
