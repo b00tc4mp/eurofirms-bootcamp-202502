54f31b94a02b37eb9fd28c861a5e20711d6cce45
Mario Garcia <mario.gaglez@gmail.com>
2025-04-17 18:06:37 +0200

    To create the Piedra-Papel-Tijera's App #3

--- staff/mario-garcia/playground/piedra-papel-tijera/src/App.jsx
+++ staff/mario-garcia/playground/piedra-papel-tijera/src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const options = ["piedra", "papel", "tijera"];
+
+export default function App() {
+
+  const [player, setPlayer] = useState("");
+  const [machine, setMachine] = useState("");
+  const [result, setResult] = useState("");
+
+  function play(selectionPlayer) {
+
+    const selectionMachine = options[Math.floor(Math.ramdom() * 3)];
+    setPlayer(selectionPlayer);
+    setMachine(selectionMachine);
+    checkWinner(selectionPlayer, selectionMachine);
+  }
+
+  function checkWinner(j, m) {
+
+    if (j === m) {
+
+      setResult("Tie");
+    }
+
+    else if (
+
+      (j === "piedra" && m === "tijera") ||
+      (j === "papel" && m === "piedra") ||
+      (j === "tijera" && m === "papel")
+    ) {
+
+      setResult("You win!");
+
+    }
+
+    else {
+
+      setResult("You lose!");
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <div className="p-4 text-center">
+
+      <h1 className="text-2xl font-bold mb-4">Piedra, Papel o Tijera</h1>
+
+      <div className="space-x-4 mb-4">
+
+        {options.map((op) => (
+
+          <button key={op} onClick={() => play(op)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+
+            {op}
+
+          </button>
+
+        ))}
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className="mb-2">You selected: <strong>{player}</strong></div>
+      <div className="mb-2">The machine selected: <strong>{machine}</strong></div>
+      <div className="text-xl font-semibold">{result}</div>
+
+
+
+    </div>
+
+  );
+
 }
 
-export default App
+
+
+
