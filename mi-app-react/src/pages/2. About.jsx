38e5c89569546228d58fd22e98a4234cc50ce4d8
EladioTech <eladio.rey@aol.com>
2025-04-24 00:36:33 +0200

    eliminada carpeta mi-app-react #41

--- mi-app-react/src/pages/2. About.jsx
+++
@@ -1,5 +0,0 @@
-function About() {
-    return <h1>Sobre nosotros</h1>;
-}
-
-export default About;
--- mi-app-react/src/pages/3. Services.jsx
+++
@@ -1,5 +0,0 @@
-function Services() {
-    return <h1>Servicios</h1>;
-}
-
-export default Services;
