2da258ab17fe5ce1e6c460e12cce32c038385b86
JAVI <javierferfer20@gmail.com>
2025-05-02 21:01:00 +0200

    actualizar app #32

--- staff/javier-fernandez/product/app/logic/getPosts.js
+++ staff/javier-fernandez/product/app/logic/getPosts.js
@@ -11,4 +11,22 @@ import { data } from '../data'
  * date: Date
  * }]} The posts from users in the system.
  */
-export const getPosts = () => data.getPosts().toReversed()
+export const getPosts = () => 
+
+    const posts = data.getPosts().toReversed()
+
+    const users = data.getPosts()
+
+    const userId = data.getUserId()
+
+    posts.forEach (post =>
+        const authorId = post.author
+        
+        const user = users.find(user => User.id === authorId)
+
+        const username = user.username
+
+        post.author = username
+
+        post.own = authorId === userId
+    )
---
+++ staff/javier-fernandez/product/zzz/app.5/logic/createPost.js
@@ -0,0 +1,35 @@
+import { data } from '../data'
+
+/**
+ * creates a post.
+ * 
+ * @param {string} image the post image url.
+ * @param {string} text the post text.
+ */
+ export const createPost = (image, text) => {
+    if (typeof image !== 'string') throw new Error('invalid image type')
+    if (!image.startsWith('http')) throw new Error('invalid image format')
+    
+    if (typeof text !== 'string') throw new Error('invalid text type')
+    if (text.length < 1) throw new Error('invalid min text length')
+    
+    let postsCount = data.getPostsCount()
+
+    postsCount++
+
+    const post = {
+        id: 'post-' + postsCount,
+        author: data.getUserId(),
+        image,
+        text,
+        date: new Date().toISOString(),
+        likes: []
+    }
+
+    const posts = data.getPosts()
+
+    posts.push(post)
+
+    data.setPosts(posts)
+    data.setPostsCount(postsCount)
+    }
