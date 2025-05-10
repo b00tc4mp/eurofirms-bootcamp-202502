8d856ed45175dbb2caf4612278873566688aee37
manuelbarzi <manuelbarzi@gmail.com>
2025-05-10 16:26:56 +0200

    add remove-post logic and test and route; add register-user and authenticate-user tests with fetch #25

--- staff/manuel-barzi/product/api/index.js
+++ staff/manuel-barzi/product/api/index.js
@@ -23,7 +23,6 @@ server.post('/users', jsonBodyParser, (request, response) => {
 server.post('/users/auth', jsonBodyParser, (request, response) => {
     try {
         const { username, password } = request.body
-
         const userId = logic.authenticateUser(username, password)
 
         response.status(200).json(userId)
@@ -35,7 +34,6 @@ server.post('/users/auth', jsonBodyParser, (request, response) => {
 server.get('/users/self/username', (request, response) => {
     try {
         const authorization = request.headers.authorization // Basic user-x
-
         const userId = authorization.slice(6)
 
         const username = logic.getUserUsername(userId)
@@ -49,7 +47,6 @@ server.get('/users/self/username', (request, response) => {
 server.post('/posts', jsonBodyParser, (request, response) => {
     try {
         const authorization = request.headers.authorization // Basic user-x
-
         const userId = authorization.slice(6)
 
         const { image, text } = request.body
@@ -65,12 +62,27 @@ server.post('/posts', jsonBodyParser, (request, response) => {
 server.get('/posts', (request, response) => {
     try {
         const authorization = request.headers.authorization // Basic user-x
-
         const userId = authorization.slice(6)
 
         const posts = logic.getPosts(userId)
 
         response.status(200).json(posts)
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
+server.delete('/posts/:postId', (request, response) => {
+    try {
+        const authorization = request.headers.authorization // Basic user-x
+        const userId = authorization.slice(6)
+
+        //const postId = request.params.postId
+        const { postId } = request.params
+
+        logic.removePost(userId, postId)
+
+        response.status(204).send()
     } catch (error) {
         response.status(500).json({ error: error.constructor.name, message: error.message })
     }
