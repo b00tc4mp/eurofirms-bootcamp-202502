54ed390aed4b9eb1e14f37347bcfddf006bcfdff
manuelbarzi <manuelbarzi@gmail.com>
2025-05-10 12:29:12 +0200

    add get-user-username logic and test and route; refactor authenticate-user and register-user with array find method; #25

--- staff/manuel-barzi/product/api/data/index.js
+++ staff/manuel-barzi/product/api/data/index.js
@@ -1,12 +1,22 @@
 import fs from 'fs'
 
 export const data = {
+    /**
+     * Saves users in database (json file).
+     * 
+     * @param {Array} users The array of users.
+     */
     setUsers(users) {
         const json = JSON.stringify(users)
 
         fs.writeFileSync('./data/users.json', json)
     },
 
+    /**
+     * Loads users from database (json file).
+     * 
+     * @returns {Array} The array of users.
+     */
     getUsers() {
         const json = fs.readFileSync('./data/users.json', 'utf8')
 
@@ -15,12 +25,22 @@ export const data = {
         return users
     },
 
+    /**
+    * Saves users count in database (json file).
+    * 
+    * @param {number} usersCount The number of users in database.
+    */
     setUsersCount(usersCount) {
         const json = JSON.stringify(usersCount)
 
         fs.writeFileSync('./data/usersCount.json', json)
     },
 
+    /**
+     * Loads users count from database (json file).
+     * 
+     * @returns {number} The number of users in database.
+     */
     getUsersCount() {
         const json = fs.readFileSync('./data/usersCount.json', 'utf8')
 
--- staff/manuel-barzi/product/api/index.js
+++ staff/manuel-barzi/product/api/index.js
@@ -32,4 +32,18 @@ server.post('/users/auth', jsonBodyParser, (request, response) => {
     }
 })
 
+server.get('/users/self/username', (request, response) => {
+    try {
+        const authorization = request.headers.authorization // Basic user-1
+
+        const userId = authorization.slice(6)
+
+        const username = logic.getUserUsername(userId)
+
+        response.status(200).json(username)
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
 server.listen(8080, () => console.log('server is up'))
