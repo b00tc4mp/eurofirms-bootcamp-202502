ea5266d636f12cd6d38e360380f23d7505a3974f
JAVI <javierferfer20@gmail.com>
2025-05-14 20:31:04 +0200

    modificar index.js #32

--- staff/javier-fernandez/product/api/index.js
+++ staff/javier-fernandez/product/api/index.js
@@ -38,13 +38,42 @@ server.post('/post', jsonBodyParser, (request, response) => {
         const authorization = request.headers.authorization // Basic user-x
         const userId = authorization.slice(6)
 
-        logic.createPost(userId, Image,text)
+        logic.createPost(userId, Image, text)
 
         response.status(201).send()
     } catch (error) {
         response.status(500).json({ error: error.constructor.name, message: error.message })
     }
-   
 })
+
+server.get('/posts', (request, response) => {
+    try {
+        const authorization = request.headers.authorization // Basic user-x
+        const userId = authorization.slice(6)
+
+        const post = logic.getPosts(userId)
+
+        resònse.status(200).json(posts)
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
+server.delete('/posts/:postId', (request, response) => {
+    try {
+        const authorization = request.headers.authorization // Basic user-x
+        const userId = authorization.slice(6)
+
+        //const postId = request.params.postId
+        const { postId } = request.params
+
+        logic.removePost(userId, postId)
+
+        response.status(204).send()
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
 
 server.listen(8080, () => console.log('server is up'))
