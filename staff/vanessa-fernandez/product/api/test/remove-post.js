8f5a4237339db9eb3813f0128ccdfc64f08d670b
mariva0280 <marivafp@gmail.com>
2025-05-16 16:31:13 +0200

    add remove-post.js to test with fetch #43

---
+++ staff/vanessa-fernandez/product/api/test/remove-post.js
@@ -0,0 +1,22 @@
+fetch('http://localhost:8080/posts/post-4', {
+    method: 'DELETE',
+    headers: {
+        'Authorization': 'Basic user-1'
+    }
+})
+    .catch(error => { throw new Error('Connection error')})
+    .then(response => {
+        const { status } = response
+
+        if(status === 204) return
+
+        return response.json()
+            .catch(error => { throw new Error('json error')})
+            .then(body => {
+                const { error, message } = body
+
+                throw new Error(message)
+            })
+    })
+    .then(() => console.log('Post deleted'))
+    .catch(error => console.error(error))
