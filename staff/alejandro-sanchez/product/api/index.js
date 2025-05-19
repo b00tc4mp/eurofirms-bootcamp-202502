e18be715fd923fe3c56fdceea822b6e17ed87067
Alejandro Sanchez <asbrelationwork@gmail.com>
2025-05-19 22:18:58 +0200

    actualizar código #45

--- staff/alejandro-sanchez/product/api/index.js
+++ staff/alejandro-sanchez/product/api/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { logic } from '../logic/index.js'
 
 const server = express()
 const jsonBodyParser = express.json()
@@ -7,29 +8,84 @@ server.get('/hello', (request, response) => {
     response.send('Hello! 😉')
 })
 
-server.get('/color', (request, response) => {
-    const q = request.query.q
+server.post('/users', jsonBodyParser, (request, response) => {
+    try {
+        const { name, email, username, password } = request.body
 
-    let code
+        logic.registerUser(name, email, username, password)
 
-    if (q === 'red')
-        code = '#FF0000'
-    else if (q === 'green')
-        code = '#00FF00'
-    else if (q === 'blue')
-        code = '#0000FF'
-    else if (q === 'yellow')
-        code = '#FFFF00'
-
-    response.send(code)
+        response.status(201).send()
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
 })
 
-server.post('/users', jsonBodyParser, (request, response) => {
-    const user = request.body
+server.post('/users/auth', jsonBodyParser, (request, response) => {
+    try {
+        const { username, password } = request.body
+        const userId = logic.authenticateUser(username, password)
 
-    console.log('user', user)
+        response.status(200).json(userId)
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
+})
 
-    response.send('user received!')
+server.get('/users/self/username', (request, response) => {
+    try {
+        const authorization = request.headers.authorization // Basic user-x
+        const userId = authorization.slice(6)
+
+        const username = logic.getUserUsername(userId)
+
+        response.status(200).json(username)
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
+server.post('/posts', jsonBodyParser, (request, response) => {
+    try {
+        const authorization = request.headers.authorization // Basic user-x
+        const userId = authorization.slice(6)
+
+        const { image, text } = request.body
+
+        logic.createPost(userId, image, text)
+
+        response.status(201).send()
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
+server.get('/posts', (request, response) => {
+    try {
+        const authorization = request.headers.authorization // Basic user-x
+        const userId = authorization.slice(6)
+
+        const posts = logic.getPosts(userId)
+
+        response.status(200).json(posts)
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
+server.delete('/posts/:postId', (request, response) => {
+    try {
+        const authorization = request.headers.authorization // Basic user-x
+        const userId = authorization.slice(6)
+
+        //const postId = request.params.postId
+        const { postId } = request.params
+
+        logic.removePost(userId, postId)
+
+        response.status(204).send()
+    } catch (error) {
+        response.status(500).json({ error: error.constructor.name, message: error.message })
+    }
 })
 
 server.listen(8080, () => console.log('server is up'))
