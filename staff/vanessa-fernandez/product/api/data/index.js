3c842e691820709b56ed89b4624d64d1066a1949
mariva0280 <marivafp@gmail.com>
2025-05-25 09:16:54 +0200

    change index.js in data for use mongoose ande delete all files .json in data folder; #43

--- staff/vanessa-fernandez/product/api/data/index.js
+++ staff/vanessa-fernandez/product/api/data/index.js
@@ -1,101 +1,11 @@
-import fs from 'fs'
+import mongoose from 'mongoose'
+import { User, Post } from './models.js'
 
-export const data = {
-    /**
-     * Saves users in database (json file).
-     * 
-     * @param {Array} users The array of users. 
-     */
-    setUsers(users) {
-        const json = JSON.stringify(users)
+const { connect, disconnect } = mongoose
+export {
+    connect,
+    disconnect,
 
-        fs.writeFileSync('./data/users.json', json)
-    },
-
-    /**
-     * Loads users from database (json file).
-     * 
-     * @returns {Array} The array of users.
-     */
-    getUsers() {
-        const json = fs.readFileSync('./data/users.json', 'utf8')
-
-        const users = JSON.parse(json)
-
-        return users
-    },
-
-    /**
-     * Saves users count in database (json file).
-     * 
-     * @param {number} usersCount The number of users in database.
-     */
-    setUsersCount(usersCount) {
-        const json = JSON.stringify(usersCount)
-
-        fs.writeFileSync('./data/usersCount.json', json)
-
-    },
-
-    /**
-     * Loads users count from database (json file).
-     * 
-     * @returns {number} The number of users in database.
-     */
-    getUsersCount() {
-        const json = fs.readFileSync('./data/usersCount.json', 'utf8')
-
-        const usersCount = JSON.parse(json)
-
-        return usersCount
-    },
-
-    /**
-     * Saves posts in database (json file).
-     * 
-     * @param {Array} posts The array of posts. 
-     */
-    setPosts(posts) {
-        const json = JSON.stringify(posts)
-
-        fs.writeFileSync('./data/posts.json', json)
-    },
-
-    /**
-     * Loads posts from database (json file).
-     * 
-     * @returns {Array} The array of posts.
-     */
-    getPosts() {
-        const json = fs.readFileSync('./data/posts.json', 'utf8')
-
-        const posts = JSON.parse(json)
-
-        return posts
-    },
-
-    /**
-     * Saves posts count in database (json file).
-     * 
-     * @param {number} postsCount The number of posts in database.
-     */
-    setPostsCount(postsCount) {
-        const json = JSON.stringify(postsCount)
-
-        fs.writeFileSync('./data/postsCount.json', json)
-    },
-
-    /**
-     * Loads posts count from database (json file).
-     * 
-     * @returns {number} The number of posts in database.
-     */
-    getPostsCount() {
-        const json = fs.readFileSync('./data/postsCount.json', 'utf8')
-
-        const postsCount = JSON.parse(json)
-
-        return postsCount
-    }
-
+    User,
+    Post
 }
