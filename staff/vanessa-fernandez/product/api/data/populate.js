9b34deb6a6b6773a9edd51c14505158e93972593
mariva0280 <marivafp@gmail.com>
2025-05-25 09:12:52 +0200

    implemnt models.js in data folder and modified populate.js in data for use mongoose; #43

--- staff/vanessa-fernandez/product/api/data/populate.js
+++ staff/vanessa-fernandez/product/api/data/populate.js
@@ -1,19 +1,62 @@
-import mongodb from 'mongodb'
+import mongoose from 'mongoose'
+import { User, Post } from './models.js'
 
-const { MongoClient, ObjectId } = mongodb
+const { connect, disconnect } = mongoose
 
-const client = new MongoClient('mongodb://localhost:27017')
+connect('mongodb://localhost:27017/test')
 
-client.connect()
     .then(() => {
-        const db = client.db('test')
-        const users = db.collection('users')
-        const posts = db.collection('posts')
+        // forma larga para crear usuario en bbdd
+        //     const user = new User({
+        //         name: 'Draco Malfoy',
+        //         email: 'draco@malfoy.com',
+        //         username: 'dracom'
+        //     })
+        //     return user.save()
+        //         .catch(error => { throw new Error(error.message)})
+        //         .then(() => console.log('user saved'))
+        
+        // forma más corta para crear usuario o lo que sea en bbdd, será la que usemos
+        
+        // return User.create({
+        //     name:'Draco Malfoy',
+        //     email:'draco@malfoy.com',
+        //     username:'dracom',
+        //     password:'789789789'
+        // })
+        //     .catch(error => { throw new Error(error.message)})
+        //     .then(() => console.log('user created'))
+       
+        //borrar usuario
+       
+        // return User.deleteOne({ _id: '6832bedd1fbafd6ade21e8f1' })
+        //     .catch(error => { throw new Error(error.message)})
+        //     .then(() => console.log('user deleted'))
+       
+        //modificar usuario
+       
+        // return User.updateOne({ _id: '6832bc19affd088cc58e24b9' }, {$set:{password:'456456456'}})
+        //     .catch(error => { throw new Error(error.message)})
+        //     .then(() => console.log('user updated'))
+        
+        // listar usuario
 
-        return users.insertOne({ name: 'George Weasly', email: 'george@weasly.com', username: 'georgeweasly', password: '123123123' })
+        // return User.find({})
+        //     .catch(error => { throw new Error(error.message)})
+        //     .then((users) => console.log('users', users))
 
-            .catch(error => { throw new Error('mongo error')})
-            .then(() => console.log('user inserted'))
+        //crear post
+
+        return Post.create({
+            author: '6831a68f74bba4476ab5b2c7',
+            image: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzkxemRiOWtjamwwY2pzcmczeXYzbTZwOGgyNnE1NWl5MTVuOGVzbSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/HATwHWlQJxKfu/giphy.gif',
+            text: 'The best day in Howgarts'
+        })
+            .catch(error => { throw new Error(error.message)})
+            .then(() => console.log('post created'))
+        // el resto de metodos creados para users se usarían igual para post o lo que sea.    
     })
+
+
     .catch(error => console.error(error))
-    .finally(() => client.close())
+    .finally(() => disconnect())
