74467095da380012c08a30e9ebb93f0b60e7628a
manuelbarzi <manuelbarzi@gmail.com>
2025-06-21 13:05:58 +0200

    move user and post routes to own routers; move json-body-parser and error-handler to middlewares folder; #25

---
+++ staff/manuel-barzi/product/zzz/api.9/logic/getPosts.js
@@ -0,0 +1,39 @@
+import { User, Post } from '../data/index.js'
+import { validate, SystemError, NotFoundError, AuthorshipError } from 'com'
+
+/**
+ * Returns post from database.
+ * 
+ * @param {string} userId The requester user id.
+ * 
+ * @returns {array} The posts from database.
+ */
+export const getPosts = userId => {
+    validate.userId(userId)
+
+    return User.findById(userId)
+        .catch(error => { throw new SystemError('mongo error') })
+        .then(user => {
+            if (!user) throw new NotFoundError('user not found')
+
+            return Post.find({}).select('-__v').populate('author', 'username').sort('-date').lean()
+                .catch(error => { throw new SystemError('mongo error') })
+                .then(posts => {
+                    posts.forEach(post => {
+                        post.id = post._id.toString()
+                        delete post._id
+
+                        if (!post.author) throw new AuthorshipError('post has not author')
+
+                        if (post.author._id) {
+                            post.author.id = post.author._id.toString()
+                            delete post.author._id
+                        }
+
+                        post.own = post.author.id === userId
+                    })
+
+                    return posts
+                })
+        })
+}
