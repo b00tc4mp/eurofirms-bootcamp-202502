95f39518f38bf22a0e8ca2527b041935481a2329
Laura Anton <lauraamps@gmail.com>
2025-06-22 16:52:22 +0200

    borados #23

--- staff/Laura-Anton/product/app/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-    envPrefix: 'VITE_',
-    plugins: [react(), tailwindcss()]
-})
