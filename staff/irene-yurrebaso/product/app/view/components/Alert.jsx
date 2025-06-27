0ac26fa34a848ddd2952466df0dbbd994e196cfe
Irene12345 <15231820+Irene12345@users.noreply.github.com>
2025-06-27 19:57:35 +0200

    change alert & confirm window style to fixed #27

--- staff/irene-yurrebaso/product/app/view/components/Alert.jsx
+++ staff/irene-yurrebaso/product/app/view/components/Alert.jsx
@@ -1,7 +1,7 @@
 export const Alert = ({ message, onAccepted }) => {
     const handleAcceptAlert = () => onAccepted()
 
-    return <div className="p-10 bg-yellow-500/70 absolute w-full h-full flex flex-col justify-center">
+    return <div className="p-10 bg-yellow-500/70 fixed w-full h-full flex flex-col justify-center">
         <div className="bg-white border-2 border-black p-2 flex flex-col gap-2">
             <p>{message}</p>
             <button className="bg-black text-white py-1 px-4 self-end" type="button" onClick={handleAcceptAlert}>Accept</button>
--- staff/irene-yurrebaso/product/app/view/components/Confirm.jsx
+++ staff/irene-yurrebaso/product/app/view/components/Confirm.jsx
@@ -3,7 +3,7 @@ export const Confirm = ({ message, onCancelled, onAccepted }) => {
 
     const handleAcceptConfirm = () => onAccepted()
 
-    return <div className="p-10 bg-yellow-500/70 absolute w-full h-full flex flex-col justify-center">
+    return <div className="p-10 bg-yellow-500/70 fixed w-full h-full flex flex-col justify-center">
         <div className="bg-white border-2 border-black p-2 flex flex-col gap-2">
             <p>{message}</p>
             <div className="self-end flex gap-4">
